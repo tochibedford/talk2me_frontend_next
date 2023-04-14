37196c3db94b124aef6ec5be2b52a6143b9cf84d
tochibedford <musicofbyte@gmail.com>
2023-04-14 16:07:27 +0100

    feat: added page loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
 import NavLayout from '@components/layouts/NavLayout'
 import '@components/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, [])
   return (
     <>
       <NavLayout>
         <Component {...pageProps} />
       </NavLayout>
+      <div id="loader" className={`${isLoading === false ? "fadeOut" : ""}`}>
+        <div className="middle">TALK2ME</div>
+      </div>
     </>
   )
 }
