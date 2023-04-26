b408bd60b6c9c2127d62f7253dd1f794e48104f1
tochibedford <musicofbyte@gmail.com>
2023-04-26 16:53:14 +0100

    feat: improved handle validation

--- components/SearchAndChat/Search/index.tsx
+++ components/SearchAndChat/Search/index.tsx
@@ -13,7 +13,7 @@ export default function Search() {
                     <legend>PUT IN A PUBLIC TWITTER HANDLE</legend>
                     <label htmlFor="search">
                         <span className={styles.atSymbol}>@</span>
-                        <input type="search" name="search" id="searchInput" onChange={(e) => setTwitterHandle(e.target.value)} value={twitterHandle} required placeholder="example = tweet like @example " />
+                        <input type="search" name="search" id="searchInput" pattern="/^[A-Za-z0-9_]{1,15}$/" title="Please enter a valid Twitter handle (e.g. username). without the '@' and no spaces" onChange={(e) => setTwitterHandle(e.target.value)} value={twitterHandle} required placeholder="tochibedford " />
                         <button type="submit" title="Generate tweets like this user ">GENERATE</button>
                     </label>
                 </fieldset>
